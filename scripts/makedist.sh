2dd8628a3c49754ae3f327cd0baecad777c0f374
Emlyn O'Regan <emlynoregan@gmail.com>
2018-02-23 16:07:37 +1030

    fix

--- scripts/makedist.sh
+++ scripts/makedist.sh
@@ -1,8 +1,8 @@
 cd src
 rm -rf dist
-echo *** python setup.py sdist
+echo --- python setup.py sdist
 python setup.py sdist
-echo *** setup.py bdist_wheel --universal
+echo --- setup.py bdist_wheel --universal
 python setup.py bdist_wheel --universal
-echo *** twine upload dist/* --skip-existing
+echo --- twine upload dist/* --skip-existing
 twine upload dist/* --skip-existing
